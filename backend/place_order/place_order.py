867430a2b64fce52353e66b4e97c96fdfbd0c6a0
Ian <ian.wong.2024@computing.smu.edu.sg>
2026-03-25 17:42:41 +0800

    updated payment to include get request for intent id from order id

--- backend/place_order/place_order.py
+++ backend/place_order/place_order.py
@@ -325,5 +325,5 @@ def getItem(itemID):
 # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
     print("This is flask " + os.path.basename(__file__) + " for placing an order...")
-    connectAMQP()
+    # connectAMQP()
     app.run(host="0.0.0.0", port=5006, debug=True)
